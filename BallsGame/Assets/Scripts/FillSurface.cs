59964dc27ab7c338ac809b41e9479d94a5756ba9
DESKTOP-6DK5DIA\tonid <tonidominguezreig@gmail.com>
2021-12-11 15:46:05 +0100

    Implements tool for vertex filling

--- BallsGame/Assets/Scripts/FillSurface.cs
+++ BallsGame/Assets/Scripts/FillSurface.cs
@@ -8,6 +8,7 @@ public class FillSurface : EditorWindow
     Object selectedSurface;
     Object objectToSpawn;
     string surfaceSpawnButton = "Fill Surface";
+    string surfaceDeleteButton = "Delete Surface Filling";
 
     [MenuItem("Tools/Fill Surface")]
     private static void OpenVertexObjectGeneratorWindow()
@@ -27,7 +28,41 @@ public class FillSurface : EditorWindow
         EditorGUILayout.Space(3);
         if (GUILayout.Button(surfaceSpawnButton))
         {
+            FillSurfaceWithGameObject();
+        }
+        EditorGUILayout.Space(2);
+        if (GUILayout.Button(surfaceDeleteButton))
+        {
+            DeleteSurfaceWithGameObject();
+        }
+    }
 
+    private void FillSurfaceWithGameObject()
+    {
+        if(selectedSurface != null && FindObjectOfType<Ball>() == false)
+        {
+            GameObject surfaceGO = (GameObject)selectedSurface;
+            Mesh mesh = surfaceGO.GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var direction = surfaceGO.transform.TransformPoint(vertices[i]);
+                GameObject instance = Instantiate((GameObject)objectToSpawn, surfaceGO.transform);
+                instance.transform.localPosition = vertices[i];
+
+                if(i == vertices.Length - 1)
+                {
+                    Debug.Log(i + " objects instantiated");
+                }
+            }
+        }
+    }
+
+    private void DeleteSurfaceWithGameObject()
+    {
+        foreach(Ball ball in FindObjectsOfType<Ball>())
+        {
+            DestroyImmediate(ball.gameObject);
         }
     }
 }
