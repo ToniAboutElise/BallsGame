ec15111f32f6b43ca81181f198efd048ded22a88
DESKTOP-6DK5DIA\tonid <tonidominguezreig@gmail.com>
2022-01-01 14:04:25 +0100

    Modifies Level 1 and starts creating method for editor window script to change selected objects into desired collectible or wall

--- BallsGame/Assets/Scripts/FillSurface.cs
+++ BallsGame/Assets/Scripts/FillSurface.cs
@@ -9,12 +9,15 @@ public class FillSurface : EditorWindow
     Object sceneObject;
     string surfaceSpawnSceneObjectsButton = "Fill Surface With Scene GameObjects";
     string surfaceDeleteButton = "Clear Scene Objects";
+    string getSelectedObjects = "Get Selected Objects";
     string clearSettersButton = "Clear Preparation Scripts";
 
     [MenuItem("Tools/Fill Surface")]
     private static void OpenVertexObjectGeneratorWindow()
     {
         FillSurface window = (FillSurface)EditorWindow.GetWindow(typeof(FillSurface));
+        window.maxSize = new Vector2(500f, 500f);
+        window.minSize = window.maxSize;
         window.Show();
     }
 
@@ -32,16 +35,37 @@ public class FillSurface : EditorWindow
             FillVerticesWithSceneObjects();
         }
         EditorGUILayout.Space(2);
-        EditorGUILayout.Space(2);
+        GUILayout.Label("Transform selected objects into any of the following options", EditorStyles.boldLabel);
+        EditorGUILayout.Space(1);
+        GUI.BeginGroup(new Rect(0, 0, 350, 350));
+
+        //GUI.Box(new Rect(250, 250, 80, 80), "Directions");
+
+        var off = 20f;
+        var px = 20f;
+        var py = 20f;
+        if(GUI.Button(new Rect(0 + off, 100 + py + off, 50, 50), "Wall"))
+        {
+            GetSelectedObjects();
+        }
+        if (GUI.Button(new Rect(50 + px + off, 100 + py + off, 50, 50), "Ball"))
+        {
+            GetSelectedObjects();
+        }
+        if (GUI.Button(new Rect(100 + px * 2f + off, 100 + py + off, 50, 50), "Bubble Ball"))
+        {
+            GetSelectedObjects();
+        }
+        GUI.EndGroup();
+        EditorGUILayout.Space(70);
+        if (GUILayout.Button(clearSettersButton))
+        {
+            ClearPreparationScripts();
+        }
         EditorGUILayout.Space(2);
         if (GUILayout.Button(surfaceDeleteButton))
         {
             ClearSurface();
-        }
-        EditorGUILayout.Space(2);
-        if (GUILayout.Button(clearSettersButton))
-        {
-            ClearPreparationScripts();
         }
 
 
@@ -64,6 +88,14 @@ public class FillSurface : EditorWindow
                     Debug.Log(i + " objects instantiated");
                 }
             }
+        }
+    }
+
+    private void GetSelectedObjects()
+    {
+        foreach (Transform transform in Selection.transforms)
+        {
+            Debug.Log(transform.name);
         }
     }
 
