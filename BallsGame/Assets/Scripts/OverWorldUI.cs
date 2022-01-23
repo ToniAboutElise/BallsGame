160f948fd41a9dc58837ed8839fd04b41a4885f5
DESKTOP-6DK5DIA\tonid <tonidominguezreig@gmail.com>
2022-01-23 23:30:29 +0100

    Fixes input bug

--- BallsGame/Assets/Scripts/OverWorldUI.cs
+++ BallsGame/Assets/Scripts/OverWorldUI.cs
@@ -7,6 +7,9 @@ public class OverWorldUI : MonoBehaviour
     [SerializeField] PlayerInputActions _playerInputActions;
     [SerializeField] Animator _overWorldAnimator;
 
+    [SerializeField] private GameObject _levelSelectionUI;
+    [SerializeField] private GameObject _customizeUI;
+
     public CurrentOverWorldSection currentOverWorldSection = CurrentOverWorldSection.LevelSelection;
     public enum CurrentOverWorldSection
     {
@@ -14,15 +17,27 @@ public class OverWorldUI : MonoBehaviour
         Customize
     }
 
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+
     private void CheckInputActions()
     {
         if (_playerInputActions.Player.L1.IsPressed())
         {
+            _overWorldAnimator.ResetTrigger("Customize");
             _overWorldAnimator.SetTrigger("LevelSelection");
+            _customizeUI.SetActive(false);
+            _levelSelectionUI.SetActive(true);
         }
         else if (_playerInputActions.Player.R1.IsPressed())
         {
+            _overWorldAnimator.ResetTrigger("LevelSelection");
             _overWorldAnimator.SetTrigger("Customize");
+            _levelSelectionUI.SetActive(false);
+            _customizeUI.SetActive(true);
         }
     }
 
